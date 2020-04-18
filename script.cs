691b86de8b7c341f305dd3605b83b4726cf35a68
secretGeek <leonbambrick@gmail.com>
2020-04-18 16:49:05 +1000

    template with more styles, moving toward embedded toc/js

---
+++ script.cs
@@ -0,0 +1,44 @@
+/* on start.... */
+// populate the breadcrumb
+// bind a 'show menu' behaviour to the hamburger menu
+
+
+
+/* utility functions */
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return (template.content.firstChild);
+}
+function $(selector) {
+    return document.querySelectorAll(selector);
+}
+function $id(id) {
+    return document.getElementById(id);
+}
+function isEmpty(obj) {
+    return (Object.keys(obj).length === 0 && obj.constructor === Object);
+}
+
+//add the class of className to all elements that match the selector
+function addClass(selector, className) {
+    for (var _i = 0, _a = $(selector); _i < _a.length; _i++) {
+        var example = _a[_i];
+        example.classList.add(className);
+    }
+}
+//remove the class className from all elements that match the selector
+function removeClass(selector, className) {
+    for (var _i = 0, _a = $(selector); _i < _a.length; _i++) {
+        var example = _a[_i];
+        example.classList.remove(className);
+    }
+}
+// remove the class of className from all elements that have a class of className
+function removeAllClass(className) {
+    for (var _i = 0, _a = $("." + className); _i < _a.length; _i++) {
+        var example = _a[_i];
+        example.classList.remove(className);
+    }
+}
