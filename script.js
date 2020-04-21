429bc95444f32d036a8cca2805650ffd37896bd0
secretGeek <leonbambrick@gmail.com>
2020-04-21 17:16:03 +1000

    copy pre on hover

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-/* on start.... */
-// populate the breadcrumb
-// bind a 'show menu' behaviour to the hamburger menu
 function configureBreadcrumb() {
 	var currentUrl = location.pathname;
 	var currentPage = currentUrl.substring(currentUrl.lastIndexOf("/") + 1);
@@ -16,49 +13,86 @@ function configureBreadcrumb() {
 	var topicLink = "<a href='" + topicTOC + "'>" + topicTitle + "</a>";
 	if (topicTitle == "today-i-learned" || topicTitle == "")
 	{
-			$id('breadcrumb').innerHTML = homeLink + joiner + "index";
+		$id('breadcrumb').innerHTML = homeLink + joiner + "index";
 	} 
 	else 
 	{
 		$id('breadcrumb').innerHTML = homeLink + joiner + topicLink + joiner + currentTitle;
 	}
 }
+
+function copyPreCodeOnClick() {
+	for(let preOrCode of $('pre, code')) {
+		preOrCode.setAttribute('title', "Click to copy.");
+		preOrCode.setAttribute('onclick', "copyItemText(this);");
+	}
+
+	for(let codeInPre of $('pre code' )) {
+		codeInPre.removeAttribute('onclick', undefined);
+	}
+}
+
+/* on start.... */
 configureBreadcrumb();
+copyPreCodeOnClick();
+
+
+function copyItemText(item) {
+	item.setAttribute('title', "Copied. Click to copy again.");
+	copytext(item.textContent);
+}
+
+function copytext(copyText) {
+	const textArea = document.createElement('textarea');
+	textArea.style.position = "absolute";
+	textArea.style.left = "-100%";
+	textArea.textContent = copyText;
+	document.body.appendChild(textArea);
+	textArea.select();
+	document.execCommand("copy");
+}
+
+
+
 /* utility functions */
 function htmlToElement(html) {
-    var template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    template.innerHTML = html;
-    return (template.content.firstChild);
+	var template = document.createElement('template');
+	html = html.trim(); // Never return a text node of whitespace as the result
+	template.innerHTML = html;
+	return (template.content.firstChild);
 }
+
 function $(selector) {
-    return document.querySelectorAll(selector);
+	return document.querySelectorAll(selector);
 }
+
 function $id(id) {
-    return document.getElementById(id);
+	return document.getElementById(id);
 }
+
 function isEmpty(obj) {
-    return (Object.keys(obj).length === 0 && obj.constructor === Object);
+	return (Object.keys(obj).length === 0 && obj.constructor === Object);
 }
 
 //add the class of className to all elements that match the selector
 function addClass(selector, className) {
-    for (var _i = 0, _a = $(selector); _i < _a.length; _i++) {
-        var example = _a[_i];
-        example.classList.add(className);
-    }
+	for (var _i = 0, _a = $(selector); _i < _a.length; _i++) {
+		var example = _a[_i];
+		example.classList.add(className);
+	}
 }
+
 //remove the class className from all elements that match the selector
 function removeClass(selector, className) {
-    for (var _i = 0, _a = $(selector); _i < _a.length; _i++) {
-        var example = _a[_i];
-        example.classList.remove(className);
-    }
+	for (var _i = 0, _a = $(selector); _i < _a.length; _i++) {
+		var example = _a[_i];
+		example.classList.remove(className);
+	}
 }
 // remove the class of className from all elements that have a class of className
 function removeAllClass(className) {
-    for (var _i = 0, _a = $("." + className); _i < _a.length; _i++) {
-        var example = _a[_i];
-        example.classList.remove(className);
-    }
+	for (var _i = 0, _a = $("." + className); _i < _a.length; _i++) {
+		var example = _a[_i];
+		example.classList.remove(className);
+	}
 }
